4ae0f2c6ee8ad9764f4a9385aaa60fe6a3195746
Luca Danelutti <lucadanelutti@outlook.com>
2023-02-07 11:15:12 +0000

    Add batch singleRun example

---
+++ examples/singleRunBatch.js
@@ -0,0 +1,17 @@
+let start;
+var engine = Polyglot.eval('gpjson', "GJ");
+engine.buildKernels();
+
+start = performance.now();
+engine.query("../datasets/twitter_small_records.json", ["$.user.lang"], true, true);
+console.log("Total time: " + (performance.now() - start) + "ms");
+
+function sleep(millis) {
+    return new Promise(resolve => setTimeout(resolve, millis));
+}
+/*
+sleep(20*1000).then(() => {
+    console.log("Exiting")
+});*/
+
+engine.close();
